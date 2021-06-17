e55914b7d501e089292975ec3e64fbb9f8cdddb6
Dennis McWilliams <d12768@gmail.com>
2021-06-17 10:44:58 -0400

    Initial

---
+++ Assets/UnityPipeServer.cs
@@ -0,0 +1,32 @@
+﻿using System.IO.Pipes;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class UnityPipeServer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Thread serverReadThread = new Thread(ServerThread_Read);
+        serverReadThread.Start();
+    }
+
+    private void ServerThread_Read(object obj)
+    {
+        Debug.Log("Server: Thread started");
+
+       NamedPipeServerStream namedPipeServerStream = new NamedPipeServerStream("ServerRead_ClientWrite", PipeDirection.In);
+
+        namedPipeServerStream.WaitForConnection();
+        Debug.Log("Server: Client has connected");
+
+        StreamString streamString = new StreamString(namedPipeServerStream);
+        string message = streamString.ReadString();
+        Debug.Log("Server: Recieved " + message);
+
+        namedPipeServerStream.Close();
+    }
+}
